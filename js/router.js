5c26189b23180b2b44d97b2287f0cad46375f36f
lubov-nefed <love.nefed@gmail.com>
2023-11-15 13:51:58 +0300

    fix: fix routing

--- js/router.js
+++ js/router.js
@@ -1,37 +1,32 @@
 const routes = {
   '/': '/pages/home/index.html',
-  '/profile-prevew': '/pages/profile-prevew/profile-prevew.html',
-  '/login-form': '/pages/login-form/login-form.html',
+  '/profile-prevew': '/pages/profile-preview/profile-preview.html',
+  '/create-account': '/pages/create-account/create-account.html',
+  '/login': '/pages/login/login.html',
 }
 
 const handleLocation = async () => {
-  console.log('handle');
   const path = window.location.pathname;
   const route = routes[path];
   const html = await fetch(route).then((data) => data.text());
-  document.querySelector('.main__container').innerHTML = html;
-
-  
+  let app = document.querySelector('.main__container');
+  app.innerHTML = html;
 }
+window.onpopstate = handleLocation;
 
 const route = (event) => {
   event.preventDefault();
-  console.log('route');
-  window.history.pushState({}, '', event.target.href || event.target.dataPath);
+  window.history.pushState({}, '', event.currentTarget.href || event.currentTarget.dataset.path);
   handleLocation();
 };
 
 
-window.onpopstate = handleLocation;
-
-window.route = route;
-
 handleLocation()
   .then(() => {
     const createBtn = document.querySelector('.create-btn');
-    const watchBtn = document.querySelector('.watch-btn');
-    if (createBtn || watchBtn) {
-      createBtn.addEventListener('click', route);
-      watchBtn.addEventListener('click', route);
-    }
+    const watchBtn = document.querySelector('.watch-btn'); 
+    const loginBtn = document.querySelector('.login-btn');
+    createBtn.addEventListener('click', route);
+    watchBtn.addEventListener('click', route);
+    loginBtn.addEventListener('click', route);
 });
